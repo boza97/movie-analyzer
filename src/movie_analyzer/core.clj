90203ae0a31aeb7cf997cb14da859dcea0fedd4d
Bozidar Mastilovic <bozidar.mastilovicc@gmail.com>
2025-12-16 09:32:22 +0100

    added more reduce functions

--- src/movie_analyzer/core.clj
+++ src/movie_analyzer/core.clj
@@ -15,11 +15,35 @@
 (defn filter-by-genre [movies genre]
   (filter (fn [m] (= (:genre m) genre)) movies))
 
+(defn sum-ratings [movies]
+  (reduce (fn [total movie] (+ total (:rating movie))) 0 movies))
+
+(defn average-rating [movies]
+  (if (empty? movies)
+    0
+    (/ (sum-ratings movies) (count movies))))
+
+(defn best-rated-movie [movies]
+  (reduce
+    (fn [best movie]
+      (if (> (:rating movie) (:rating best)) movie best))
+    (first movies)
+    movies))
+
+(defn count-high-rated [movies min-rating]
+  (reduce
+    (fn [acc movie]
+      (if (> (:rating movie) min-rating) (+ acc 1) acc))
+    0 movies))
+
 (defn -main
   []
   (println "Movies titles:" (get-titles movies))
   (println "Total number of movies:" (count movies))
   (println "Sci-Fi movies:" (get-titles (filter-by-genre movies "Sci-Fi")))
+  (println "Average movies rating:" (average-rating movies))
+  (println "Best rated movie:" (:title (best-rated-movie movies)))
+  (println "Number of movies with rating higher then 8.8:" (get-titles (count-high-rated movies 8.8)))
   )
 
 
