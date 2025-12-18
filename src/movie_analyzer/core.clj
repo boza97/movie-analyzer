ea5b41c213476646ab101f586e745058f6fb7a7c
Bozidar Mastilovic <bozidar.mastilovicc@gmail.com>
2025-12-18 12:32:21 +0100

    added loading movies from csv file and logic for parsing it

--- src/movie_analyzer/core.clj
+++ src/movie_analyzer/core.clj
@@ -1,19 +1,88 @@
-(ns movie-analyzer.core)
+(ns movie-analyzer.core
+  (:require [clojure.string :as str]))
+
+(def filename "resources/imdb_top_1000.csv")
+(def movie-keys
+  [:title :year :genres :rating :director :votes :gross])
+
+(defn select-columns [row]
+  [(nth row 1)                                              ; title
+   (nth row 2)                                              ; year
+   (nth row 5)                                              ; genres
+   (nth row 6)                                              ; rating
+   (nth row 9)                                              ; director
+   (nth row 14)                                             ; votes
+   (nth row 15)])                                           ; gross
+
+(defn clean-string [s]
+  (str/replace s #"^\"|\"$" ""))
+
+(defn clean-number-string [s]
+  (str/replace
+    (str/replace
+      (str/replace s "\"" "")
+      "," "")
+    "\r" ""))
+
+(defn str->int-safe [s]
+  (if (or (nil? s) (= (clean-string s) "PG") (= (clean-number-string s) ""))
+    nil
+    (Integer/parseInt (clean-number-string s))))
+
+(defn str->double-safe [s]
+  (if (or (nil? s) (= s ""))
+    nil
+    (Double/parseDouble s)))
+
+(defn str->genres [s]
+  (str/split s #",\s*"))
+
+(def conversions
+  {:title    identity
+   :year     str->int-safe
+   :genres   str->genres
+   :rating   str->double-safe
+   :director identity
+   :votes    str->int-safe
+   :gross    str->int-safe})
+
+(defn convert [movie-key value]
+  ((get conversions movie-key) (clean-string value)))
+
+(defn parse [string]
+  (map
+    (fn [line]
+      (str/split
+        line
+        #",(?=(?:[^\"]*\"[^\"]*\")*[^\"]*$)"))
+    (str/split string #"\n")))
+
+(defn mapify [rows]
+  (map
+    (fn [row]
+      (reduce
+        (fn [row-map pair]
+          (assoc row-map
+            (first pair)
+            (convert (first pair) (second pair))))
+        {}
+        (map vector movie-keys row)))
+    rows))
 
 (def movies
-  [{:title "Inception" :year 2010 :genre "Sci-Fi" :rating 8.8}
-   {:title "The Godfather" :year 1972 :genre "Crime" :rating 9.2}
-   {:title "Interstellar" :year 2014 :genre "Sci-Fi" :rating 8.6}
-   {:title "The Dark Knight" :year 2008 :genre "Action" :rating 9.0}
-   {:title "Pulp Fiction" :year 1994 :genre "Crime" :rating 8.9}
-   {:title "The Matrix" :year 1999 :genre "Sci-Fi" :rating 8.7}
-   {:title "Parasite" :year 2019 :genre "Thriller" :rating 8.6}
-   {:title "Spirited Away" :year 2001 :genre "Animation" :rating 8.6}])
+  (mapify
+    (map select-columns
+         (rest
+           (parse
+             (slurp filename))))))
+
 
 (defn get-titles [movies] (map :title movies))
 
 (defn filter-by-genre [movies genre]
-  (filter (fn [m] (= (:genre m) genre)) movies))
+  (filter
+    (fn [m] (some (fn [g] (= g genre)) (:genres m)))
+    movies))
 
 (defn sum-ratings [movies]
   (reduce (fn [total movie] (+ total (:rating movie))) 0 movies))
@@ -43,7 +112,7 @@
   (println "Sci-Fi movies:" (get-titles (filter-by-genre movies "Sci-Fi")))
   (println "Average movies rating:" (average-rating movies))
   (println "Best rated movie:" (:title (best-rated-movie movies)))
-  (println "Number of movies with rating higher then 8.8:" (get-titles (count-high-rated movies 8.8)))
+  (println "Number of movies with rating higher then 8.8:" (count-high-rated movies 8.8))
   )
 
 
